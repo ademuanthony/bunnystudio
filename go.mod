80bf492e335205439bea8807923a1d7dc1ee2736
Ademu Anthony <ademuanthony@gmail.com>
2021-01-24 18:07:26 +0100

    Dynamodb store for user service

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module bonnystudio.com/taskmanager
 go 1.13
 
 require (
+	github.com/aws/aws-sdk-go v1.35.17
 	github.com/go-kit/kit v0.10.0
+	github.com/guregu/dynamo v1.10.2
 	github.com/pborman/uuid v1.2.1
 )
---
+++ pkg/users/dynamodb.go
@@ -0,0 +1,64 @@
+package users
+
+import (
+	"bonnystudio.com/taskmanager/internal"
+	"bonnystudio.com/taskmanager/internal/util"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/guregu/dynamo"
+)
+
+const tableName = "Users"
+
+type dynamoStore struct {
+	table dynamo.Table
+}
+
+func NewDynamoStore() *dynamoStore {
+	endpoint := "http://localhost:8000"
+	db := dynamo.New(session.New(), &aws.Config{
+		Region:   aws.String("us-west-2"),
+		Endpoint: &endpoint,
+	})
+
+	if err := db.CreateTable(tableName, internal.User{}).Run(); err != nil {
+		logger.Log("CreateTable", err)
+	}
+
+	return &dynamoStore{table: db.Table(tableName)}
+}
+
+func (s *dynamoStore) Create(user internal.User) error {
+	return s.table.Put(user).Run()
+}
+
+func (s *dynamoStore) Update(user internal.User) error {
+	err := s.table.Update(user.ID, user).Run()
+	if err == dynamo.ErrNotFound {
+		err = util.ErrUnknown
+	}
+	return err
+}
+
+func (s *dynamoStore) FindByID(userID string) (*internal.User, error) {
+	var user internal.User
+	err := s.table.Get("ID", userID).One(&user)
+	if err != nil {
+		if err == dynamo.ErrNotFound {
+			err = util.ErrUnknown
+		}
+		return nil, err
+	}
+	return &user, nil
+}
+
+func (s *dynamoStore) GetAll() ([]internal.User, error) {
+	var users []internal.User
+	err := s.table.Scan().All(&users)
+
+	return users, err
+}
+
+func (s *dynamoStore) Delete(userID string) error {
+	return s.table.Delete("ID", userID).Run()
+}
