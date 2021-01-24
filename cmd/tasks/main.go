fb72ac572b94acfc10527a8a71b1f40d2efb0a45
Ademu Anthony <ademuanthony@gmail.com>
2021-01-24 18:53:48 +0100

    Setup and start tasks service

---
+++ cmd/tasks/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"bonnystudio.com/taskmanager/pkg/tasks"
+	"bonnystudio.com/taskmanager/pkg/tasks/endpoints"
+	"bonnystudio.com/taskmanager/pkg/tasks/transport"
+
+	"github.com/go-kit/kit/log"
+	"github.com/oklog/oklog/pkg/group"
+)
+
+const (
+	defaultHTTPPort = "8081"
+)
+
+func main() {
+	var (
+		logger   log.Logger
+		httpAddr = net.JoinHostPort("localhost", envString("HTTP_PORT", defaultHTTPPort))
+	)
+
+	logger = log.NewLogfmtLogger(log.NewSyncWriter(os.Stderr))
+	logger = log.With(logger, "ts", log.DefaultTimestampUTC)
+
+	var (
+		dynamoStore = tasks.NewDynamoStore()
+		service     = tasks.NewService(dynamoStore)
+		eps         = endpoints.NewEndpointSet(service)
+		httpHandler = transport.NewHTTPHandler(eps)
+	)
+
+	var g group.Group
+	{
+		// The HTTP listener mounts the Go kit HTTP handler we created.
+		httpListener, err := net.Listen("tcp", httpAddr)
+		if err != nil {
+			logger.Log("transport", "HTTP", "during", "Listen", "err", err)
+			os.Exit(1)
+		}
+		g.Add(func() error {
+			logger.Log("transport", "HTTP", "addr", httpAddr)
+			return http.Serve(httpListener, httpHandler)
+		}, func(error) {
+			httpListener.Close()
+		})
+	}
+	{
+		// This function just sits and waits for ctrl-C.
+		cancelInterrupt := make(chan struct{})
+		g.Add(func() error {
+			c := make(chan os.Signal, 1)
+			signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+			select {
+			case sig := <-c:
+				return fmt.Errorf("received signal %s", sig)
+			case <-cancelInterrupt:
+				return nil
+			}
+		}, func(error) {
+			close(cancelInterrupt)
+		})
+	}
+	logger.Log("exit", g.Run())
+}
+
+func envString(env, fallback string) string {
+	e := os.Getenv(env)
+	if e == "" {
+		return fallback
+	}
+	return e
+}
